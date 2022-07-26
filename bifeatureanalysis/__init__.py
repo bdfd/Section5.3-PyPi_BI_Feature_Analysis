267517dbc47b537401f91f0a29c1fb37bc0d6ace
bdfd <bdfd2005@gmail.com>
2022-07-26 12:46:19 -0400

    init header

--- bifeatureanalysis/__init__.py
+++ bifeatureanalysis/__init__.py
@@ -1,10 +1,14 @@
 '''
-Author: BDFD
-Date: 2021-10-27 18:42:50
-LastEditTime: 2022-07-26 11:00:21
-LastEditors: BDFD
-Description: In User Settings Edit
-FilePath: \Section5.3-PyPi_Feature_Engineering\bifeatureanalysis\__init__.py
+Date         : 2022-07-25 17:48:24
+Author       : BDFD,bdfd2005@gmail.com
+Github       : https://github.com/bdfd
+LastEditTime : 2022-07-26 12:38:37
+LastEditors  : BDFD
+Description  : 
+FilePath     : \bifeatureanalysis\__init__.py
+Copyright (c) 2022 by BDFD, All Rights Reserved. 
 '''
 
+
 from bifeatureanalysis.templateproj import add_one
+from bifeatureanalysis.aggregationfunc import add_one
